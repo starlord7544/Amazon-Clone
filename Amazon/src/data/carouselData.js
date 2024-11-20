7906523dea37c820a568a7a34b7c5fe16ca8e0de
starlord7544 <154723727+starlord7544@users.noreply.github.com>
2024-11-21 01:54:26 +0530

    Add files via upload

---
+++ Amazon/src/data/carouselData.js
@@ -0,0 +1,18 @@
+export const carouselData = [
+  {
+    image: 'https://images-eu.ssl-images-amazon.com/images/G/31/2024/LandG/GW/November/SSD/revise/1500_X_600-GW-Heros-Pc-01._CB541876061_.jpg',
+    alt: 'Sustainable decor days'
+  },
+  {
+    image: 'https://images-eu.ssl-images-amazon.com/images/G/31/prime/ACQ/BAU/PC/Hero/HO/PSE/PSE_PC_3000X1200_T3._CB567196011_.jpg',
+    alt: 'Limited period offer'
+  },
+  {
+    image: 'https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/OnePlus/Flagship/OP12_NewLaunch/14thNov24/20thNov24/D108792004_WLD_OnePlus_Waffle_NewLaunch_DesktopTall_Hero_3000x1200._CB542081718_.jpg',
+    alt: 'OnePlus 12 Offer'
+  },
+  {
+    image: 'https://images-eu.ssl-images-amazon.com/images/G/31/img21/APAY/AugART/travel/IF_2_PC_Hero_3000x1200._CB541138782_.jpg',
+    alt: 'Dubai flights'
+  }
+];
